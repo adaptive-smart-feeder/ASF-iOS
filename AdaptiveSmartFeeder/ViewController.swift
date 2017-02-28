0fb95f50d55f7828527625126291caf4c76edbd7
Vitorvgc <vitorvgc07@gmail.com>
2017-02-28 18:36:26 -0300

    First commit

---
+++ AdaptiveSmartFeeder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveSmartFeeder
+//
+//  Created by Vítor Chagas on 28/02/17.
+//  Copyright © 2017 Adaptive Samrt Feeder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
